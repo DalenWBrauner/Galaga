ba4cebf2fc7241621daa8df48ff06012696c6dd7
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-10-06 15:28:07 -0400

    Rewrote comment regarding [=]
    Ah, I forgot to update this comment after I figured out what [=] meant.

--- Galaga Source Code/Galaga Source Code/Player.cpp
+++ Galaga Source Code/Galaga Source Code/Player.cpp
@@ -4,7 +4,7 @@
 template <typename GameObjectType, typename Function>
 std::function<void(SceneNode&, sf::Time)> derivedAction(Function fn) {
 
-	// Okay, don't know what [=] means.
+	// We return "[=]". [=] translates to: a "captured" copy of Function's return value
 	return[=](SceneNode& node, sf::Time dt) {
 
 		// Checks if we can cast our node as <GameObjectType>
